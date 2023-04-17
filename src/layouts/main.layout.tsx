0b419a470cc702b2c3ac836d3d8b80e5100c72e1
pravtz <ederson.ufrgs@gmail.com>
2023-04-17 19:43:48 -0300

    add manifest and create favicon

--- src/layouts/main.layout.tsx
+++ src/layouts/main.layout.tsx
@@ -8,6 +8,12 @@ export const MainLayout = ({ children }: { children: React.ReactNode }) => {
         <title>My Blog | Pravtz </title>
         <meta charSet="utf-8" />
         <meta property="og:title" content="My Blog" key="title" />
+        <meta name='description' content='Blog e página pessoal de Ederson Pravtz' />
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        <meta
+          property='og:description'
+          content='Blog e página pessoal de Ederson Pravtz'
+        />
       </Head>
       <div>
         {children}
