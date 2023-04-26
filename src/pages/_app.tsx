90c69bee9e270ad30f825a3998287e096d56e01b
pravtz <ederson.ufrgs@gmail.com>
2023-04-26 20:36:35 -0300

    add analytics vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { Analytics } from '@vercel/analytics/react'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -24,6 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
       />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
