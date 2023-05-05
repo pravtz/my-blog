d1c3d713ae179f0024696fa847f0e50899f80541
pravtz <ederson.ufrgs@gmail.com>
2023-05-04 23:33:56 -0300

    fix imge

--- src/components/ProfileCover/index.tsx
+++ src/components/ProfileCover/index.tsx
@@ -1,19 +1,16 @@
 import Image from 'next/image'
 import { SolcialMedia } from '@/components/SocialMedia'
 import clsx from 'clsx'
+import Avatar from '/public/avatar.png'
 
 export const ProfileCover = () => {
   return (
 
     <div className='flex flex-col w-full items-center text-[#bbb] justify-center mx-auto relative'>
-
-      <div className=' rounded-full w-[260px] mb-4 bg-cover'>
-        <Image src='/avatar.png' alt='avatar' width='276' height='276' />
-      </div>
-
-      <p className='text-md'>Olá, Bem vindo! Meu nome é</p>
+      <Image className='rounded-full object-contain mb-4' src={Avatar} alt='avatar' width='260' height='260' />
+      <p className='text-md text-[#848484]'>Olá, Bem vindo! Meu nome é</p>
       <h1 className='text-[32px] md:text-[52px]'><span className=' font-bold'>Ederson </span>Pravtz</h1>
-      <p className=' text-base'>Sou desenvolvedor de sistemas web</p>
+      <p className=' text-base text-[#848484]'>Sou desenvolvedor de sistemas web</p>
 
       <div className='py-4'>
         <SolcialMedia />
