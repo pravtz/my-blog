5d11088abe9f71fda3437faa566a3bf29e338f2f
pravtz <ederson.ufrgs@gmail.com>
2023-06-16 15:29:46 -0300

    corretion layers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,17 +110,17 @@ export default function Page() {
   ]
   return (
     <div className="relative ">
-      <div className=" bg-gradient-bg-top absolute top-0 h-screen w-full  z-auto " />
+      <div className=" bg-gradient-bg-top absolute top-0 h-screen w-full  -z-10 " />
       <header className=" w-full ">
-        <div className="bg-[#7D7C79] opacity-[0.18] w-full h-[200px] " />
+        <div className="bg-[#7D7C79] opacity-[0.18] w-full h-[200px] -z-10" />
         <div className="relative h-[calc(100vh-200px)] ">
 
           <div className="absolute top-[-100px] w-full left-0">
-            <div className="absolute opacity-50 w-[300px] h-[300px] bottom-[-10%] bg-gradient-center left-[calc(50%-150px)] z-auto" />
+            <div className="absolute opacity-50 w-[300px] h-[300px] bottom-[-10%] bg-gradient-center left-[calc(50%-150px)] -z-10" />
             <ProfileCover />
           </div>
 
-          <div className="absolute text-center animate-bounce bottom-[15%] w-full">
+          <div className="absolute text-center animate-bounce bottom-[10%] w-full z-10  ">
             <a href="#myprojects">
               <ButtonIcon
                 className="hover:opacity-50"
