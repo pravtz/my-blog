a374e949fd432627dac08fce4dfb2336349407f5
pravtz <ederson.ufrgs@gmail.com>
2023-06-16 15:12:47 -0300

    custon data tag

--- src/components/TagCardProject/index.tsx
+++ src/components/TagCardProject/index.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from "react"
+import { DataTagCardProject } from "./dataTagCardProject"
+import { Search } from "lucide-react"
 
 type TagCardProjectProps = {
   tags: string[]
@@ -6,10 +8,25 @@ type TagCardProjectProps = {
 }
 
 export const TagCardProject = ({tags, children}: TagCardProjectProps) => {
+  const dataKey = Object.keys(DataTagCardProject)
+  const dataValue = Object.values(DataTagCardProject)
+ 
+   const filterTagName = (arr: typeof dataValue, search: string ) => {
+      return arr.filter((value)=> value.nameSearch === search)
+  }
+
   return (
     <div className="relative">
       <div  className={" absolute top-6 right-2 flex flex-col gap-1 items-end"}>
-        {tags && tags.length > 0 && tags.map((item,index) => {
+        {tags && tags.length > 0 && tags.map((item) => {
+          if(dataKey.includes(item))  {
+            const objValue = filterTagName(dataValue, item)
+            return (
+              <div className=" drop-shadow-[-3px_2px_2px_rgba(0,0,0,0.5)] pl-2 pr-4 py-1 bg-primary text-white text-[12px] w-fit rounded-l-[4px]" key={item}>
+              {objValue[0].name}
+            </div>
+            )
+          }
           return (
               <div className=" drop-shadow-[-3px_2px_2px_rgba(0,0,0,0.5)] pl-2 pr-4 py-1 bg-primary text-white text-[12px] w-fit rounded-l-[4px]" key={item}>
                 {item.replace("_"," ")}
