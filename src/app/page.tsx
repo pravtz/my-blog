5fee2b02555b8847ae1892182b31eb3b2a9b9cc2
pravtz <ederson.ufrgs@gmail.com>
2023-06-22 13:05:09 -0300

    add balancer for style text and create variable textcontribuition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import { WrapperCardProjects } from "@/components/cardProjetos/WrapperCardProjects";
 import { TitleMain } from "@/components/TitleMain";
 import { ScreenContriburionBlog } from "@/components/ScreenContriburionBlog";
+import BalancerText from "react-wrap-balancer"
 
 export const metadata: Metadata = {
   title: 'MyBlog | Pravtz',
@@ -12,6 +13,7 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
+  const textContribuition = "Evoluir, contribuir e compartilhar é importante porque é a chave para o progresso. Ao evoluir nossas habilidades e conhecimentos, somos capazes de criar soluções cada vez mais eficazes e eficientes para os problemas que enfrentamos. Compartilhar essas habilidades e conhecimentos com outras pessoas nos permite inspirá-las a fazer o mesmo, e juntos, podemos alcançar mais do que jamais poderíamos sozinhos. É através da evolução e compartilhamento que podemos moldar um futuro melhor para nós mesmos e para o mundo."
 
   return (
     <div className="relative ">
@@ -54,7 +56,7 @@ export default function Page() {
         >
           <div className="max-w-3xl mx-auto">
             <TitleMain title="Contribuições" />
-            <p className="text-center text-lg leading-8 font-sans font-light text-white/60 my-8">Evoluir, contribuir e compartilhar é importante porque é a chave para o progresso e inovação. Ao evoluir nossas habilidades e conhecimentos, somos capazes de criar soluções cada vez mais eficazes e eficientes para os problemas que enfrentamos. Compartilhar essas habilidades e conhecimentos com outras pessoas nos permite inspirá-las a fazer o mesmo, e juntos, podemos alcançar mais do que jamais poderíamos sozinhos. É através da evolução e compartilhamento que podemos moldar um futuro melhor para nós mesmos e para o mundo.</p>
+            <p className="text-center text-lg leading-8 font-sans font-light text-white/60 my-8"><BalancerText>{textContribuition}</BalancerText></p>
             <ScreenContriburionBlog />
           </div>
         </section>
